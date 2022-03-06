d206aa935dd6436500da84dc83471f1f8d2e3a85
Jan <jan@JR-MBP.fritz.box>
2022-03-06 19:12:15 +0100

    Commit starter file

---
+++ main.py
@@ -0,0 +1,16 @@
+from data import Data
+from selenium import webdriver
+##########
+##########
+##########
+class ApartmentBot():
+    def __init__(self):
+        pass
+
+
+
+##########
+##########
+##########
+if __name__ == "__main__":
+    ApartmentBot()
